3adf4df26bfe4dc7e74835865cd28cc6b72d0654
STG7 <shauryaggamer@gmail.com>
2023-01-23 21:06:55 -0800

    asdf

--- model/nflnews.py
+++ model/nflnews.py
@@ -16,9 +16,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class NFLNews(db.Model):
     __tablename__ = 'NFL News'  
 
-    # Defining the User Constructor
+    # Defining the Object Variable
 
-    id = db.Column(db.Integer, primary_key=True)
+
     _teams = db.Column(db.String(255), unique=False, nullable=False)
     _score = db.Column(db.String(255), unique=False, nullable=False)
     _type = db.Column(db.String(255), unique=False, nullable=False)
@@ -27,12 +27,14 @@ class NFLNews(db.Model):
     # Constructor of a User object, initializes the instance variables within object (self)
 
     def __init__(own, teams, score, type, day=date.today()):
+
         own._teams = teams    # variables with "own"" prefix become part of the object
         own._score = score
         own._type = type
         own._day = day
 
     # Getter and setter methods for all variables
+
     @property
     def teams(own):
         return own._teams
@@ -43,15 +45,21 @@ class NFLNews(db.Model):
     
     @property
     def score(own):
-        return own._
+        return own._score
     
     @score.setter
     def score(own, score):
         own.score = score
-        
 
-    def is_score(own, score):
-        return own._score == score
+
+    @property
+    def type(own):
+        return own.type
+    
+    @type.setter
+    def score(own, type):
+        own.type = type
+
       
     @property
     def day(own):
@@ -63,18 +71,13 @@ class NFLNews(db.Model):
     def day(own, day):
         own._day = day
     
-    @property
-    def age(own):
-        today = date.today()
-        return today.year - own._day.year - ((today.month, today.day) < (own._day.month, own._day.day))
     
-    # output content using str(object) in human readable form, uses getter
-    # output content using json dumps, this is ready for API response
+    
+  
     def __str__(own):
         return json.dumps(own.read())
 
-    # CRUD create/add a new record to the table
-    # returns self or None on error
+    
     def create(own):
         try:
             # creates a person object from User(db.Model) class, passes initializers
@@ -85,31 +88,17 @@ class NFLNews(db.Model):
             db.session.remove()
             return None
 
-    # CRUD read converts self to dictionary
-    # returns dictionary
+
     def read(own):
         return {
 
             "teams": own.teams,
             "score": own.score,
-            "age": own.age,
+            "type": own.type,
             "day": own.day
             
         }
-
-    # CRUD update: updates user name, password, phone
-    # returns self
-    def update(own, teams="", score=""):
-        """only updates values with length"""
-        if len(teams) > 0:
-            own.teams = teams
-        if len(score) > 0:
-            own.score = score
-        db.session.commit()
-        return own
-
-    # CRUD delete: remove self
-    # None
+    
     def delete(own):
         db.session.delete(own)
         db.session.commit()
@@ -118,26 +107,17 @@ class NFLNews(db.Model):
 
 """Database Creation and Testing """
 
-
 # Builds working data for testing
+
 def initNFLNews():
     """Create database and tables"""
     db.create_all()
     """Tester data for table"""
-    u1 = NFLNews(title='Bolsonaro supporters storm Brazilian Congress.', network='CNN', day=date(2023, 1, 21))
-    u2 = NFLNews(title='Kevin McCarthy is new speaker', network='Fox', day=date(2023, 1, 20))
-    u3 = NFLNews(title='Woman sentenced to three years in state prison for collecting 400,000 in viral GoFundMe scam', network='ABC', day=date(2023, 1, 19))
-    u4 = NFLNews(title='Ukraine denies Russian claim it killed 600 soldiers', network='NBC', day=date(2023, 1, 20))
-    u5 = NFLNews(title='Damar Hamlin: Buffalo Bills make stirring display in support of safety during victory', network='BBC', day=date(2023, 1, 22))
-    u6 = NFLNews(title='Worshippers in Tokyo plunge into ice bath to mark new year', network='CNN', day=date(2023, 1, 21))
-    u7 = NFLNews(title='Driver crashes and flips vehicle inside drive-through car wash', network='Fox', day=date(2023, 1, 20))
-    u8 = NFLNews(title='Brazilian police fire tear gas at Bolsonaro supporters', network='ABC', day=date(2023, 1, 19))
-    u9 = NFLNews(title='Deer rescued from frozen river in Wisconsin', network='NBC', day=date(2023, 1, 20))
-    u10 = NFLNews(title='Two years after Covid food still tastes rotten', network='BBC', day=date(2023, 1, 22))
+    u1 = NFLNews(teams = 'Bufallo Bils vs Cincanatti Bengals', score='10:27', type = 'Divisonal Round', day=date(2023, 1, 22))
 
     
-    nfl_news = [u1, u2, u3, u4, u5, u6, u7, u8, u9, u10]
-    # breaking_news = [u1, u2, u3, u4, u5]
+    nfl_news = [u1]
+
 
     """Builds sample user/note(s) data"""
     for news in nfl_news:
